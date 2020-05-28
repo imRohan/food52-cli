23b5d28dce8668e059fccbb4167aaf55fdd0ec35
Rohan Likhite <contact@rohanlikhite.com>
2020-05-28 11:45:28 -0400

    added prompt as module var, show description

--- lib/chef.rb
+++ lib/chef.rb
@@ -7,29 +7,46 @@ require_relative 'cookbook'
 include Cookbook
 
 module Chef
-  def self.ask
-    prompt = TTY::Prompt.new
-    
-    name = prompt.ask('What would you like to eat today?')
-    recipes = Cookbook.search(name)
+  @@prompt = TTY::Prompt.new
 
-    recipe_name = prompt.select("Heres what I found, which one would you like to read?", recipes.keys)
+  def self.ask
+    keywords = @@prompt.ask('What would you like to eat today?')
+    Chef.present(keywords)
+  end
+
+  def self.present(keywords) 
+    recipes = Cookbook.search(keywords)
+
+    recipe_name = @@prompt.select("Found #{recipes.length} recipes:", recipes.keys)
     recipe = Cookbook.get(recipes[recipe_name])
+
+    description, ingredients, steps = recipe.values_at(:description, :ingredients, :steps)
 
     recipe_table = Terminal::Table.new do | table |
       table.title = recipe_name
-      table.add_row [ { value: 'Ingredients', alignment: :center } ]
-      recipe[:ingredients].each do | ingredient |
+      table.add_row [ { value: "Ingredients - #{ingredients.length} total", alignment: :center } ]
+      ingredients.each do | ingredient |
         table.add_row [ ingredient ]
       end
     end
-
     puts recipe_table
 
-    puts "---Steps---"
-    recipe[:steps].each.with_index(1) do | step, index |
+    puts "\n"
+    puts 'Description'
+    puts description
+
+    puts "\n"
+    puts 'Steps'
+    steps.each.with_index(1) do | step, index |
       puts "#{index}) #{step} \n"
     end
+
+    restart = @@prompt.yes?("Would you like to see more #{keywords} recipes?")
+    restart ? self.present(keywords) : self.end()
+  end
+
+  def self.end
+    puts 'Ok Bye'
   end
 end
 
