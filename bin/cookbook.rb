c89f9d0d3c1acb31f018dd81813f5ef204c3ba99
Rohan Likhite <contact@rohanlikhite.com>
2020-05-27 22:36:53 -0400

    got majority of content displaying

--- bin/cookbook.rb
+++ bin/cookbook.rb
@@ -1,16 +1,49 @@
 # External Deps
 require 'http'
+require 'nokogiri'
+
 include HTTP
+include Nokogiri
 
 module Cookbook
   def search (keyword)
-    puts "Searching for #{keyword}"
-
     begin
-      raw_response = HTTP.get("https://food52.com/recipes/search?q=#{keyword}")
-      puts raw_response
+      recipes = Hash.new()
+
+      raw_response = HTTP.get("https://food52.com/recipes/search?q=#{keyword}").to_s
+      html_document = Nokogiri::HTML.parse(raw_response)
+
+      html_document.css('div.card__details h3 a:first-child').each do | recipe |
+        title = recipe['title']
+        link = recipe['href']
+        recipes.store(title, link)
+      end
+      
+      recipes
     rescue Exception => error
       puts "Failed to search for recipes: #{error}" 
+    end
+  end
+
+  def get (recipe_link)
+    begin
+      recipe = { ingredients: [], steps: [] }
+      raw_response = HTTP.get("https://food52.com#{recipe_link}").to_s
+      html_document = Nokogiri::HTML.parse(raw_response)
+
+      html_document.css('div.recipe__list.recipe__list--ingredients > ul > li').each do | ingredient |
+        item = ingredient.text.strip.delete("\n")
+        recipe[:ingredients].push(item)
+      end
+
+      html_document.css('div.recipe__list.recipe__list--steps > ol > li').each do | step |
+        step = step.text.strip.delete("\n")
+        recipe[:steps].push(step)
+      end
+
+      return recipe
+    rescue Exception => error
+      puts "Failed to fetch recipe: #{error}" 
     end
   end
 end
--- bin/recipes.rb
+++ bin/recipes.rb
@@ -10,4 +10,19 @@ include Cookbook
 prompt = TTY::Prompt.new
 
 name = prompt.ask('What would you like to eat today?')
-Cookbook.search(name)
+recipes = Cookbook.search(name)
+
+recipe_link = prompt.select("Heres what I found, which one would you like to read?", recipes)
+recipe = Cookbook.get(recipe_link)
+
+puts "Ok, here is the recipe"
+
+puts "---Ingredents---"
+recipe[:ingredients].each do | ingredient |
+  puts ingredient
+end
+
+puts "---Steps---"
+recipe[:steps].each do | step |
+  puts step
+end
