a2c76a1059602405d97515ba4e297e957b0491a6
Rohan Likhite <contact@rohanlikhite.com>
2020-06-01 15:19:20 -0400

    added logic to search recipes via main ingredient, made code DRY

--- lib/chef.rb
+++ lib/chef.rb
@@ -10,18 +10,54 @@ require_relative 'cookbook'
 
 # Presents the user with conversation style interface to find recipes
 module Chef
+  @options = ['Search via keywords', 'By main ingredient'] 
   @prompt = TTY::Prompt.new
 
-  def self.ask
-    keywords = @prompt.ask('What would you like to eat today?')
-    Chef.present(keywords)
+  def self.init
+    option = @prompt.select('How would you like to search for recipes?', @options)
+
+    parse_selection(option)
   end
 
-  def self.present(keywords)
-    recipes = Cookbook.search(keywords)
+  def self.parse_selection(option)
+    keywords, ingredient = @options
 
+    case option
+    when keywords
+      ask_for_keywords
+    when ingredient
+      show_ingredients
+    else
+      raise "Received invalid option: #{option}"
+    end
+  rescue StandardError => e
+    puts "Failed to receive a proper response: #{e}"
+  end
+
+  def self.ask_for_keywords
+    keywords = @prompt.ask('What would you like to eat today?')
+    find_recipe_by_keywords(keywords)
+  end
+
+  def self.show_ingredients
+    ingredients = Cookbook.ingredients
+    ingredient_name = @prompt.select('Select main ingredient', ingredients.keys)
+    find_recipe_by_ingredient(ingredients[ingredient_name])
+  end
+
+  def self.find_recipe_by_keywords(keywords)
+    recipes = Cookbook.search_by_keywords(keywords)
+    present_recipes(recipes)
+  end
+
+  def self.find_recipe_by_ingredient(ingredient_link)
+    recipes = Cookbook.search_by_ingredient(ingredient_link)
+    present_recipes(recipes)
+  end
+
+  def self.present_recipes(recipes)
     recipe_name = @prompt.select("Found #{recipes.length} recipes:", recipes.keys)
-    recipe = Cookbook.get(recipes[recipe_name])
+    recipe = Cookbook.show_recipe(recipes[recipe_name])
 
     description, ingredients, steps = recipe.values_at(:description, :ingredients, :steps)
 
@@ -50,8 +86,8 @@ module Chef
       puts "#{index}) #{step_wraped} \n"
     end
 
-    restart = @prompt.yes?("Would you like to see more #{keywords} recipes?")
-    restart ? present(keywords) : self.end
+    restart = @prompt.yes?('Would you like to keep looking?')
+    restart ? init : self.end
   end
 
   def self.end
--- lib/cookbook.rb
+++ lib/cookbook.rb
@@ -4,16 +4,35 @@
 require 'http'
 require 'nokogiri'
 
-include HTTP
-include Nokogiri
-
 # Searches and fetches recipes from food website, Food52.com
 module Cookbook
-  def self.search(keywords)
+  include HTTP
+  include Nokogiri
+
+  @base_url = 'https://food52.com'
+
+  def self.search_by_keywords(keywords)
     keywords_formatted = keywords.strip.gsub(/\s/, '%20')
 
-    raw_response = HTTP.get("https://food52.com/recipes/search?q=#{keywords_formatted}&o=popular").to_s
-    html_document = Nokogiri::HTML.parse(raw_response)
+    raw_response = HTTP.get("#{@base_url}/recipes/search?q=#{keywords_formatted}&o=popular").to_s
+
+    recipes = Cookbook.parse_recipes(raw_response)
+    recipes
+  rescue StandardError => e
+    puts "Failed to search for recipes: #{e}"
+  end
+
+  def self.search_by_ingredient(ingredient_link)
+    raw_response = HTTP.get("#{@base_url}#{ingredient_link}").to_s
+
+    recipes = Cookbook.parse_recipes(raw_response)
+    recipes
+  rescue StandardError => e
+    puts "Failed to search for recipes by ingredient: #{e}"
+  end
+
+  def self.parse_recipes(raw_html)
+    html_document = Nokogiri::HTML.parse(raw_html)
 
     recipes = {}
     html_document.css('div.card__details h3 a:first-child').each do |recipe|
@@ -23,30 +42,40 @@ module Cookbook
     end
 
     recipes
-  rescue StandardError => e
-    puts "Failed to search for recipes: #{e}"
   end
 
-  def self.get(recipe_link)
-    recipe = { description: '', ingredients: [], steps: [] }
-
-    raw_response = HTTP.get("https://food52.com#{recipe_link}").to_s
+  def self.show_recipe(recipe_link)
+    raw_response = HTTP.get("#{@base_url}#{recipe_link}").to_s
     html_document = Nokogiri::HTML.parse(raw_response)
 
-    html_document.css('div.recipe__list.recipe__list--ingredients > ul > li').each do |ingredient|
-      item = ingredient.text.gsub(/\R+/, ' ').strip
-      recipe[:ingredients].push(item)
+    ingredients = html_document.css('div.recipe__list.recipe__list--ingredients > ul > li').map do |ingredient|
+      ingredient.text.gsub(/\R+/, ' ').strip
     end
 
-    html_document.css('div.recipe__list.recipe__list--steps > ol > li').each do |step|
-      step = step.text.strip.delete("\n")
-      recipe[:steps].push(step)
+    steps = html_document.css('div.recipe__list.recipe__list--steps > ol > li').map do |step|
+      step.text.strip.delete("\n")
     end
 
-    recipe[:description] = html_document.css('div.recipe__text > div.recipe__notes > p').text.strip
+    description = html_document.css('div.recipe__text > div.recipe__notes > p').text.strip
 
-    recipe
+    { description: description, ingredients: ingredients, steps: steps }
   rescue StandardError => e
     puts "Failed to fetch recipe: #{e}"
+  end
+
+  def self.ingredients
+    raw_response = HTTP.get("#{@base_url}/recipes/ingredient/all").to_s
+    html_document = Nokogiri::HTML.parse(raw_response)
+
+    ingredients = {}
+    html_document.css('div.recipe-tags__heading.content__container > ul > li > a').each do |ingredient|
+      title = ingredient.text
+      link = ingredient['href']
+      ingredients.store(title, link)
+    end
+
+    ingredients
+  rescue StandardError => e
+    puts "Failed to fetch ingredients: #{e}"
   end
 end
