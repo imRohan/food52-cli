c445b130178bfdfa28cc272a4aa739c98d7ed1d5
Rohan Likhite <contact@rohanlikhite.com>
2020-06-02 14:19:13 -0400

    fixed linting

--- bin/food52-cli.rb
+++ bin/food52-cli.rb
@@ -1,7 +1,7 @@
 #!usr/bin/env ruby
 # frozen_string_literal: true
 
-$: << File.join(File.dirname(__FILE__), "/../lib")
+$LOAD_PATH << File.join(File.dirname(__FILE__), '/../lib')
 require 'chef'
 
 begin
